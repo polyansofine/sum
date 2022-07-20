c093bbcb8440b33eb97f5a8c3614dc9d6da8a35e
Yan <93109473+polyansofine@users.noreply.github.com>
2022-07-20 17:42:49 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+fn main() {
+    // println!("Hello, world!");
+    let mut first = String::new();
+    let mut second = String::new();
+    println!("Please enter a first name");
+    io::stdin().read_line(&mut first);
+    let mut a:u32 = 0;
+    match first.trim().parse() {
+        Ok(val) => {
+            a = val;
+        },
+        Err(_err) => {
+            println!("this is not valid type number");
+        }
+    }
+    println!("Please enter a second name");
+    io::stdin().read_line(&mut second);
+    let mut b:u32 = 0;
+    match second.trim().parse() {
+        Ok(val) => {
+            b=val;
+        },
+        Err(_err) => {
+            println!("this is not second valid type number");
+        }
+    }
+    let result = sum(a,b);
+    println!("{} + {} = {}",a,b,result);
+}
+
+fn sum(a:u32,b:u32) -> u32 {
+    a+b
+}
