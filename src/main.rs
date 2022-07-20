4258f8d332a14f29961e6e8d8af8d0f2c0f12ffe
Yan <93109473+polyansofine@users.noreply.github.com>
2022-07-20 18:20:13 +0300

    DRY

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,15 @@
 use std::io;
 use std::process;
+
 fn main() {
     loop {
         
-        let mut first = String::new();
-        let mut second = String::new();
+       
         println!("Please enter a first name");
-        io::stdin().read_line(&mut first).unwrap();
-        let  a:u32 ;
-        match first.trim().parse() {
-            Ok(val) => {
-                a = val;
-            },
-            Err(_err) => {
-                println!("this is not valid type number");
-                process::exit(1);
-            }
-        }
+        let a = read_input_user();
         println!("Please enter a second name");
-        io::stdin().read_line(&mut second).unwrap();
-        let  b:u32 ;
-        match second.trim().parse() {
-            Ok(val) => {
-                b=val;
-            },
-            Err(_err) => {
-                println!("this is not second valid type number");
-                process::exit(1);
-            }
-        }
-        let result = sum(a,b);
+        let b =  read_input_user();
+        let result = sum(a, b);
         println!("{} + {} = {}",a,b,result);
     }
     // println!("Hello, world!");
@@ -37,4 +17,19 @@ fn main() {
 
 fn sum(a:u32,b:u32) -> u32 {
     a+b
+}
+fn read_input_user() -> u32 {
+    let mut input=String::new();
+    io::stdin().read_line(&mut input).unwrap();
+        let  digit:u32 ;
+        match input.trim().parse() {
+            Ok(val) => {
+                digit = val;
+            },
+            Err(_err) => {
+                println!("this is not valid type number");
+                process::exit(1);
+            }
+        }
+        digit
 }
